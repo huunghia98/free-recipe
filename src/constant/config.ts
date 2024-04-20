71eeae4f2fe3f622514f3d074fa10294f9bcbcdc
nghiaNH <nghianh.it@tripi.vn>
2024-04-20 14:32:01 +0700

    update

--- src/constant/config.ts
+++ src/constant/config.ts
@@ -1,11 +1,11 @@
 export const siteConfig = {
-  title: `${process.env.NEXT_PUBLIC_SITE_NAME} - Read novels online and free with swift updates`,
+  title: `${process.env.NEXT_PUBLIC_SITE_NAME} - Free recipe for meals`,
   description:
-    'Explore our collection of free online books! Dive into a world of daily updates with a wide array of novels including light novel translations, web novels, Chinese, English, Japanese, and Korean novels available for your reading pleasure',
+    'Discover Delicious Recipes. Explore a world of culinary inspiration',
   /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
   url: process.env.NEXT_PUBLIC_SITE_URL ?? '',
   siteName: process.env.NEXT_PUBLIC_SITE_NAME ?? '',
   beautyAddressName: process.env.NEXT_PUBLIC_SITE_DOMAIN_NAME ?? '',
-  contactEmail: 'contact@antsnovel.com',
-  fbPage: 'https://www.facebook.com/antsnovel',
+  contactEmail: 'nghianh.w@gmai.com',
+  fbPage: 'https://www.facebook.com/huunghia198',
 };
